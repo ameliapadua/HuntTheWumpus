250541121e6abb8b6d89a1f2f4e3ac3036c730ff
edwardcrapino <edwardcrapino@gmail.com>
2012-12-05 17:17:04 -0800

    Create Hostage.cs

---
+++ Hostage.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace HuntTheTerrorist
+{
+  class Hostage
+	{
+		public bool alive;
+		public int hostageRoomNumber;
+
+		public Hostage()
+		{
+			this.alive = true;
+		}
+
+		public int GetHostageRoomNumber()
+		{
+			return hostageRoomNumber;
+		}
+
+		public bool IsHostageAlive()
+		{
+			return alive;
+		}
+
+		//If the hostage is "grenaded"
+		public void KilledByGrenade()
+		{
+			this.alive = false;
+		}
+	}
+}
